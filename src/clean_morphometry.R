bd73891cf7cd290127d26ce70b46a0f50b64edbd
Nepo <braulio.rojas@islas.org.mx>
2020-03-31 13:31:58 -0700

    Agrega instrucciones para simular una CLI

--- src/clean_morphometry.R
+++ src/clean_morphometry.R
@@ -3,8 +3,30 @@
 # Hace el análisis exploratorio de datos de roedores 
 
 # Librarias necesarias
+library(optparse)
 library(tidyverse)
 
-morfometria <- read_csv("data/raw/morfometria_gatos_erradicacion_isla_guadalupe_ISO8601.csv")
+option_list = list(
+    make_option(
+        c("-d", "--data"), 
+        type="character", 
+        default=NULL, 
+        help="nombre del archivo de entrada", 
+        metavar="character"
+    ),
+    make_option(
+        c("-o", "--out"), 
+        type="character", 
+        default="hola", 
+        help="nombre del archivo de salida", 
+        metavar="character"
+    )
+); 
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+input_file <- opt$data
+morfometria <- read_csv(input_file)
 morfometria_limpio <- morfometria %>% filter(Fecha > as.Date("2018-04-23"))
-write_csv(morfometria_limpio, "morfometria_limpio.csv")
+output_file <- opt$out
+write_csv(morfometria_limpio, output_file)
--- src/get_captures.R
+++ src/get_captures.R
@@ -3,8 +3,30 @@
 # Hace el análisis exploratorio de datos de roedores 
 
 # Librarias necesarias
+library(optparse)
 library(tidyverse)
 
-posicion_trampas <- read_csv("data/raw/posicion_trampas_gatos_isla_guadalupe.csv")
+option_list = list(
+    make_option(
+        c("-d", "--data"), 
+        type="character", 
+        default=NULL, 
+        help="nombre del archivo de entrada", 
+        metavar="character"
+    ),
+    make_option(
+        c("-o", "--out"), 
+        type="character", 
+        default="hola", 
+        help="nombre del archivo de salida", 
+        metavar="character"
+    )
+); 
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+input_file <- opt$data
+posicion_trampas <- read_csv(input_file)
 capturas_posicion_trampa <- posicion_trampas %>% filter(Estado_trampa == "X")
-write_csv(capturas_posicion_trampa, "capturas_posicion.csv")
+output_file <- opt$out
+write_csv(capturas_posicion_trampa, output_file)
