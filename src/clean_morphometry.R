188e837e7ab1ab0c1f9933c3314be9fe574cbc68
Maritza <maritza.bello@islas.org.mx>
2020-03-31 12:51:57 -0700

    Cambia archivo que lee la función

--- src/clean_morphometry.R
+++ src/clean_morphometry.R
@@ -5,6 +5,6 @@
 # Librarias necesarias
 library(tidyverse)
 
-morfometria <- read_csv("data/raw/morfometria_gatos_erradicacion_isla_guadalupe.csv")
+morfometria <- read_csv("data/raw/morfometria_gatos_erradicacion_isla_guadalupe_ISO8601.csv")
 morfometria_limpio <- morfometria %>% filter(Fecha > as.Date("2018-04-23"))
 write_csv(morfometria_limpio, "morfometria_limpio.csv")
