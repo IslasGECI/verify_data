b13c75354130ddafcf342b8a5cc41c948f24b35a
Nepo <braulio.rojas@islas.org.mx>
2020-06-10 09:54:14 -0700

    Agrega la dirección correcta de los datos

--- tests/testthat/test_check_columns_name.R
+++ tests/testthat/test_check_columns_name.R
@@ -1,8 +1,9 @@
 source("../../src/check_columns_name.R")
 archivo_salida <- '../data_tests/column_name_data_test.csv'
 salida_test <- readr::read_csv(archivo_salida)
-archivo_prueba <- '../data_tests/esfuerzo_data_tests.csv'
+archivo_prueba <- '../data/IG_POSICION_TRAMPAS_10MAY2020.csv'
 datos_prueba <- readr::read_csv(archivo_prueba)
+
 test_that("Detecte los nombres de las columnas que están incorrectas", {
     expect_equal(check_columns_name(datos_prueba), salida_test)
 })
