5bbe6ec513f88b507e11af21f5b255ffb40a43a7
Nepo <braulio.rojas@islas.org.mx>
2020-06-15 15:12:18 -0700

    Agrega función y prueba del reporsitorio datatools

---
+++ src/effort_2_tidy.R
@@ -0,0 +1,31 @@
+#!/usr/bin/env Rscript
+#
+# 
+library(tidyverse)
+
+effort_2_tidy <- function(datos_prueba){
+filter_table <- datos_prueba %>% select(c(2, 6:12))
+result <- data.frame()
+ncolumns <- length(filter_table)
+for (i in ncolumns:2){
+  cats <- which(filter_table[i] == "X")
+  if (length(cats) == 1){
+    if (length(result) == 0){
+      result <- cbind(filter_table[cats, 1], filter_table[cats, 2], names(filter_table[2]))
+      j <- 3
+    }else{
+      j <- 2  
+    }
+    for (i in j:ncolumns){
+      captures <- cbind(filter_table[cats, 1], filter_table[cats, i], names(filter_table[i]))
+      names(result) <- names(captures) 
+      result <- rbind(result, captures)
+    }
+  }
+}
+names(result) <- c("ID", "estado", "fecha")
+for (i in 1:3){
+result[i] <- as.factor(result[, i])
+}
+return(result)
+}
---
+++ tests/testthat/test_effort_2_tidy.R
@@ -0,0 +1,11 @@
+source("../../src/effort_2_tidy.R")
+salida_test <- data.frame(
+    ID = rep(c('TC-20-1084', 'TC-10-0980-MV'),each = 7),
+    estado = c('A', 'A', 'X', 'A', 'A', 'A', 'A', 'X', 'A', 'A', 'A', 'A', 'D', 'D'), 
+    fecha = rep(c('4/27/2020', '4/28/2020', '4/29/2020', '4/30/2020', '5/1/2020','5/2/2020','5/3/2020'), 2)
+)
+archivo_prueba <- '../data_tests/esfuerzo_data_tests.csv'
+datos_prueba <- readr::read_csv(archivo_prueba)
+test_that("Cambia las palabras a otras cambiando el número de sus letras", {
+    expect_equal(effort_2_tidy(datos_prueba), salida_test)
+})
