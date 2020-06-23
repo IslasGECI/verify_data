236c71e933c01ba6fa4ee23dbd3b0ee4f22dea63
Genaro Velazquez <genaro.velazquez@islas.org.mx>
2020-06-23 12:18:28 -0700

    Convierte en version CLI

--- src/make_check_header.R
+++ src/make_check_header.R
@@ -1,11 +1,34 @@
 #!/usr/bin/env Rscript
 #
 #
+
 source("src/check_columns_name.R")
 
 library(tidyverse)
-archivo_entrada <- "tests/data/IG_POSICION_TRAMPAS_10MAY2020.csv"
-datos_prueba <- readr::read_csv(archivo_entrada)
+library(optparse)
 
-salida <- check_columns_name(datos_prueba)
-print(salida)
+# Sección de la CLI
+listaOpciones = list(
+    make_option(
+        c("-d", "--data"),
+        default=NULL,
+        help="nombre del archivo de entrada",
+        metavar="character",
+        type="character"
+    ),
+    make_option(
+        c("-s", "--salida"),
+        default=NULL,
+        help="nombre del archivo de salida",
+        metavar="character",
+        type="character"
+    )
+)
+opt_parser <- OptionParser(option_list = listaOpciones);
+opciones <- parse_args(opt_parser);
+
+input_file <- read_csv(opciones$data)
+data <- readr::read_csv(input_file)
+table <- check_columns_name(data)
+output_file <- opciones$salida
+write.table(table, output_file)
