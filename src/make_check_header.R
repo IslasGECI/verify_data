4c027228891463040797c5da9f0748c2e1e23785
Genaro Velazquez <genaro.velazquez@islas.org.mx>
2020-06-23 14:13:22 -0700

    Agrega mensaje para cuando todas los encabezados están correctos

--- src/make_check_header.R
+++ src/make_check_header.R
@@ -30,6 +30,10 @@ opciones <- parse_args(opt_parser)
 input_file <- opciones$data
 data <- readr::read_csv(input_file)
 table <- check_columns_name(data)
-print(table)
+if (all(table$es_correcto)){
+	print("Todo esta correcto")
+}else{
+	print(table)
+}
 output_file <- opciones$salida
 write_csv(table, output_file)
