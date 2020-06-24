f9c6db1f8939ca32b53c68637b25fe0c8326cd8e
Nepo <braulio.rojas@islas.org.mx>
2020-06-24 11:22:54 -0700

    Refactoriza la prueba

--- tests/bashtest/test_get_captures.py
+++ tests/bashtest/test_get_captures.py
@@ -1,11 +1,8 @@
 import subprocess
 from bashtest import *
+
 def test_get_captures():
-    set_get_captures()
+    set_tests_from_make("reports/tables/cleaned_position_traps.csv")
     file_new = "./reports/tables/cleaned_position_traps.csv"
     file_test = "./tests/data_tests/cleaned_position_traps_test.csv"
     assert_equal_file(file_new, file_test)
-
-def set_get_captures():
-    bash_command = f"make reports/tables/cleaned_position_traps.csv"
-    subprocess.getoutput(bash_command)
