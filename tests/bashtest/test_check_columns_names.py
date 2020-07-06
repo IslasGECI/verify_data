e045ea0c82be6be23cfed752df1ecc15d991268b
Genaro Velazquez <genaro.velazquez@islas.org.mx>
2020-07-06 09:17:03 -0700

    Actualiza los archivos que se comparan

--- tests/bashtest/test_check_columns_names.py
+++ tests/bashtest/test_check_columns_names.py
@@ -3,10 +3,10 @@ from bashtest import *
 
 def test_check_columns_name():
     set_check_columns_name()
-    file_new = "./reports/tables/cleaned_position_traps.csv"
-    file_test = "./tests/data_tests/cleaned_position_traps_test.csv"
+    file_new = "./reports/tables/check_header_test.csv"
+    file_test = "./tests/data_tests/check_header_test.csv"
     assert_equal_file(file_new, file_test)
 
 def set_check_columns_name():
-    bash_command = "src/make_check_header.R --data tests/data/IG_POSICION_TRAMPAS_10MAY2020.csv"
+    bash_command = "src/make_check_header.R --data tests/data/IG_POSICION_TRAMPAS_10MAY2020.csv > reports/tables/check_header_test.csv"
     subprocess.getoutput(bash_command)
