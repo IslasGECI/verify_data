d58403e0e3cc6321346e95b7370aa16f434b5190
Genaro Velazquez <genaro.velazquez@islas.org.mx>
2020-07-29 15:06:58 -0700

    Imprime tabla en terminal

--- src/make_table_tidy.R
+++ src/make_table_tidy.R
@@ -15,19 +15,10 @@ listaOpciones = list(
         help="nombre del primer archivo de entrada",
         metavar="character",
         type="character"
-    ),
-    make_option(
-        c("-s", "--salida"),
-        default=NULL,
-        help="nombre del archivo de salida",
-        metavar="character",
-        type="character"
-    )
-);
+))
 opt_parser <- OptionParser(option_list = listaOpciones);
 opciones <- parse_args(opt_parser);
 
 data <- read_csv(opciones$data)
 diferencias <- diferenciasMorfometriaPosicionTrampas::effort_2_tidy(data)
-salida <- opciones$salida
-write_csv(diferencias, salida)
+print(diferencias)
