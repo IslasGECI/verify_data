ea77007bf98afbdb1083d38b219d9bda667047b5
Genaro Velazquez <genaro.velazquez@islas.org.mx>
2020-07-29 15:02:44 -0700

    Modifica encabezados de tabla prueba

--- tests/testthat/test_effort_2_tidy.R
+++ tests/testthat/test_effort_2_tidy.R
@@ -3,8 +3,8 @@ library(tidyverse)
 
 salida_test <- data.frame(
     ID = rep(c('TC-20-1084', 'TC-10-0980-MV'),each = 7),
-    estado = c('A', 'A', 'X', 'A', 'A', 'A', 'A', 'X', 'A', 'A', 'A', 'A', 'D', 'D'), 
-    fecha = rep(c('4/27/2020', '4/28/2020', '4/29/2020', '4/30/2020', '5/1/2020','5/2/2020','5/3/2020'), 2)
+    Estado_trampa = c('A', 'A', 'X', 'A', 'A', 'A', 'A', 'X', 'A', 'A', 'A', 'A', 'D', 'D'), 
+    Fecha = rep(c('4/27/2020', '4/28/2020', '4/29/2020', '4/30/2020', '5/1/2020','5/2/2020','5/3/2020'), 2)
 )
 archivo_prueba <- '../data_tests/esfuerzo_data_tests.csv'
 datos_prueba <- readr::read_csv(archivo_prueba)
