b49e8efbd1f030a97cad17546dcf9875435ecf48
Genaro Velazquez <genaro.velazquez@islas.org.mx>
2020-08-12 12:51:10 -0700

    Cambia encabezado de data.frame de prueba a ID_de_trampa

--- tests/testthat/test_effort_2_tidy.R
+++ tests/testthat/test_effort_2_tidy.R
@@ -2,21 +2,20 @@ library(diferenciasMorfometriaPosicionTrampas)
 library(tidyverse)
 
 salida_test <- data.frame(
-    ID = rep(c('TC-20-1084', 'TC-10-0980-MV'),each = 7),
+    ID_de_trampa = rep(c('TC-20-1084', 'TC-10-0980-MV'),each = 7),
     Estado_trampa = c('A', 'A', 'X', 'A', 'A', 'A', 'A', 'X', 'A', 'A', 'A', 'A', 'D', 'D'), 
     Fecha = rep(c('4/27/2020', '4/28/2020', '4/29/2020', '4/30/2020', '5/1/2020','5/2/2020','5/3/2020'), 2)
 )
 salida_test_2 <- data.frame(
-    ID = rep(c('TC-20-1084', 'TC-20-1085', 'TC-10-0980-MV'),each = 7),
+    ID_de_trampa = rep(c('TC-20-1084', 'TC-20-1085', 'TC-10-0980-MV'),each = 7),
     Estado_trampa = c('A', 'A', 'X', 'A', 'A', 'A', 'A','A', 'A', 'X', 'A', 'A', 'A', 'A', 'X', 'A', 'A', 'A', 'A', 'D', 'D'), 
     Fecha = rep(c('4/27/2020', '4/28/2020', '4/29/2020', '4/30/2020', '5/1/2020','5/2/2020','5/3/2020'), 3)
 )
 salida_test_3 <- data.frame(
-    ID = rep(c('TC-20-1084', 'TC-20-1085', 'TC-10-0980-MV'),each = 7),
+    ID_de_trampa = rep(c('TC-20-1084', 'TC-20-1085', 'TC-10-0980-MV'),each = 7),
     Estado_trampa = c('A', 'A', 'X', 'A', 'A', 'A', 'A','A', 'A', 'X', 'A', 'A', 'X', 'A', 'X', 'A', 'A', 'A', 'A', 'D', 'D'), 
     Fecha = rep(c('4/27/2020', '4/28/2020', '4/29/2020', '4/30/2020', '5/1/2020','5/2/2020','5/3/2020'), 3)
 )
-archivo_prueba <- '../data_tests/esfuerzo_data_tests.csv'
 archivo_prueba <- '../data_tests/esfuerzo_data_tests.csv'
 datos_prueba <- readr::read_csv(archivo_prueba)
 archivo_prueba <- '../data_tests/esfuerzo_data_tests_2.csv'
