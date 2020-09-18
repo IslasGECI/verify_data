09e690f0a161c1d4da629bad5ab2c8c4c0929a29
Maritza <maritza.bello@islas.org.mx>
2020-09-18 14:22:42 -0700

    Pruevba las clases

--- tests/testthat/test_check_columns_name.R
+++ tests/testthat/test_check_columns_name.R
@@ -4,7 +4,7 @@ library(diferenciasMorfometriaPosicionTrampas)
 library(reshape2)
 
 archivo_salida <- '../data_tests/column_name_data_test.csv'
-salida_test <- readr::read_csv(archivo_salida)
+salida_test <- tibble::as_tibble(readr::read_csv(archivo_salida))
 archivo_prueba <- '../data/IG_POSICION_TRAMPAS_10MAY2020.csv'
 datos_prueba <- readr::read_csv(archivo_prueba)
 obtained <- diferenciasMorfometriaPosicionTrampas::check_columns_name(datos_prueba)
@@ -12,6 +12,16 @@ nrow_obtained <- nrow(obtained)
 nrow_expected <- 11
 ncol_obtained <- ncol(obtained)
 ncol_expected <- 2
+type_obtained <- class(obtained)
+type_expected <- class(salida_test)
+first_column_type_obtained <- typeof(obtained[,1])
+first_column_type_expected <- typeof(salida_test[,1])
+
+
+test_that("Que el tipo sea el esperado", {
+    expect_equal(type_expected, type_obtained)
+    expect_equal(first_column_type_obtained,first_column_type_expected)
+})
 
 test_that("Que la forma sea la correcta", {
     expect_equal(nrow_expected, nrow_obtained)
