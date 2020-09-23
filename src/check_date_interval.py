a5cb805a22ad308ef687897a755beb8332f62b9f
Maritza <maritza.bello@islas.org.mx>
2020-09-23 11:51:38 -0700

    Corrige importación  de `date_interval_tools`

---
+++ src/check_date_interval.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from cli_paths import path_files
+from date_interval_tools import check_date_interval
+
+path = path_files()
+file_path = path.input[0][0]
+filename = path.input[1][0]
+check_date_interval(file_path,filename)
