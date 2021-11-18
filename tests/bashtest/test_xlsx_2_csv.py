42b7d36da6508f1e013017c6760f22773f172a88
MaritzaB <maritza.bello@islas.org.mx>
2021-11-18 11:56:05 -0800

    💅 make format with black

--- tests/bashtest/test_xlsx_2_csv.py
+++ tests/bashtest/test_xlsx_2_csv.py
@@ -1,10 +1,13 @@
 import subprocess
 
+
 def test_xlsx_2_csv():
-    bash_command = "./src/xlsx_2_csv tests/data/datos_formato_excel/IG_POSICION_TRAMPAS_10MAY2020.xlsx"
+    bash_command = (
+        "./src/xlsx_2_csv tests/data/datos_formato_excel/IG_POSICION_TRAMPAS_10MAY2020.xlsx"
+    )
     subprocess.getoutput(bash_command)
     bash_command = "md5sum tests/data/datos_formato_excel/IG_POSICION_TRAMPAS_10MAY2020.csv"
     output = subprocess.getoutput(bash_command)
     obtained_md5 = output.split(" ")[0]
     expected_md5 = "7421a0fea98ebe58d73465373992be48"
-    assert obtained_md5==expected_md5
+    assert obtained_md5 == expected_md5
