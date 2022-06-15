ede0d7c3452fab16e0a47fe34334191b86a0b51d
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-06-15 11:05:53 -0700

    🧪 Test that  does not return empty columns

--- tests/bashtest/test_xlsx_2_csv.py
+++ tests/bashtest/test_xlsx_2_csv.py
@@ -11,3 +11,12 @@ def test_xlsx_2_csv():
     obtained_md5 = output.split(" ")[0]
     expected_md5 = "7421a0fea98ebe58d73465373992be48"
     assert obtained_md5 == expected_md5
+    bash_command = (
+        "./src/xlsx_2_csv tests/data/datos_formato_excel/IG_MORFOMETRIA_GATOS_12JUN2022.xlsx"
+    )
+    subprocess.getoutput(bash_command)
+    bash_command = "md5sum tests/data/datos_formato_excel/IG_MORFOMETRIA_GATOS_12JUN2022.csv"
+    output = subprocess.getoutput(bash_command)
+    obtained_md5 = output.split(" ")[0]
+    expected_md5 = "7cd3b0bd106d08eec93527b2bf3c9c85"
+    assert obtained_md5 == expected_md5
