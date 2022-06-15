bb317f116dbf43e6fdabae9ccff08b8843b91847
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-06-15 14:23:54 -0700

    ✅ Update hash

--- tests/bashtest/test_xlsx_2_csv.py
+++ tests/bashtest/test_xlsx_2_csv.py
@@ -12,10 +12,9 @@ def test_xlsx_2_csv():
     expected_md5 = "7421a0fea98ebe58d73465373992be48"
     assert obtained_md5 == expected_md5
 
+
 def test_cleaned_morphometry_xlsx_2_csv():
-    bash_command = (
-        "./src/cleaned_morphometry_xlsx_2_csv.sh tests/data/datos_formato_excel/IG_MORFOMETRIA_GATOS_12JUN2022.xlsx"
-    )
+    bash_command = "./src/cleaned_morphometry_xlsx_2_csv.sh tests/data/datos_formato_excel/IG_MORFOMETRIA_GATOS_12JUN2022.xlsx"
     subprocess.getoutput(bash_command)
     bash_command = "md5sum tests/data/datos_formato_excel/IG_MORFOMETRIA_GATOS_12JUN2022.csv"
     output = subprocess.getoutput(bash_command)
@@ -23,13 +22,12 @@ def test_cleaned_morphometry_xlsx_2_csv():
     expected_md5 = "7cd3b0bd106d08eec93527b2bf3c9c85"
     assert obtained_md5 == expected_md5
 
+
 def test_cleaned_position_xlsx_2_csv():
-    bash_command = (
-        "./src/cleaned_position_xlsx_2_csv.sh tests/data/datos_formato_excel/IG_POSICION_TRAMPAS_12JUN2022.xlsx"
-    )
+    bash_command = "./src/cleaned_position_xlsx_2_csv.sh tests/data/datos_formato_excel/IG_POSICION_TRAMPAS_12JUN2022.xlsx"
     subprocess.getoutput(bash_command)
     bash_command = "md5sum tests/data/datos_formato_excel/IG_POSICION_TRAMPAS_12JUN2022.csv"
     output = subprocess.getoutput(bash_command)
     obtained_md5 = output.split(" ")[0]
-    expected_md5 = "1d5fac68689a9054593314aa56af59e9"
+    expected_md5 = "4fd30809660722b7bef86b3216b00d30"
     assert obtained_md5 == expected_md5
