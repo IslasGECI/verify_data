348fb9f114fee99b7c59711c428470899de4e945
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-06-15 12:29:22 -0700

    🧪 Test

--- tests/bashtest/test_xlsx_2_csv.py
+++ tests/bashtest/test_xlsx_2_csv.py
@@ -22,3 +22,14 @@ def test_cleaned_morphometry_xlsx_2_csv():
     obtained_md5 = output.split(" ")[0]
     expected_md5 = "7cd3b0bd106d08eec93527b2bf3c9c85"
     assert obtained_md5 == expected_md5
+
+def test_cleaned_position_xlsx_2_csv():
+    bash_command = (
+        "./src/cleaned_position_xlsx_2_csv.sh tests/data/datos_formato_excel/IG_POSICION_TRAMPAS_12JUN2022.xlsx"
+    )
+    subprocess.getoutput(bash_command)
+    bash_command = "md5sum tests/data/datos_formato_excel/IG_POSICION_TRAMPAS_12JUN2022.csv"
+    output = subprocess.getoutput(bash_command)
+    obtained_md5 = output.split(" ")[0]
+    expected_md5 = "1d5fac68689a9054593314aa56af59e9"
+    assert obtained_md5 == expected_md5
