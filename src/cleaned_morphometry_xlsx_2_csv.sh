e2c4ffa36de59d228bdd048d4aa79cc72fd55edf
Mario Villasante <mario.villasante@islas.org.mx>
2022-06-17 13:01:14 -0700

    ♻🔥 Refactor unused code

--- src/cleaned_morphometry_xlsx_2_csv.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#
-#Convierte archivo xlsx a csv
-data_file=${1}
-src_path=$(dirname "${data_file}")
-table_name=$(basename "${data_file}" .xlsx)
-in2csv --no-header-row --blanks ${data_file} | tail --lines=+2 > ${src_path}/${table_name}.tmp
-csvcut -c 1-31 -x ${src_path}/${table_name}.tmp > ${src_path}/${table_name}.csv
--- tests/bashtest/test_xlsx_2_csv.py
+++ tests/bashtest/test_xlsx_2_csv.py
@@ -10,23 +10,3 @@ def test_clean_xlsx_2_csv():
     obtained_md5 = output.split(" ")[0]
     expected_md5 = "4fd30809660722b7bef86b3216b00d30"
     assert obtained_md5 == expected_md5
-
-
-def test_cleaned_morphometry_xlsx_2_csv():
-    bash_command = "./src/cleaned_morphometry_xlsx_2_csv.sh tests/data/datos_formato_excel/IG_MORFOMETRIA_GATOS_12JUN2022.xlsx"
-    subprocess.getoutput(bash_command)
-    bash_command = "md5sum tests/data/datos_formato_excel/IG_MORFOMETRIA_GATOS_12JUN2022.csv"
-    output = subprocess.getoutput(bash_command)
-    obtained_md5 = output.split(" ")[0]
-    expected_md5 = "7cd3b0bd106d08eec93527b2bf3c9c85"
-    assert obtained_md5 == expected_md5
-
-
- def test_cleaned_position_xlsx_2_csv():
-     bash_command = "./src/cleaned_position_xlsx_2_csv.sh tests/data/datos_formato_excel/IG_POSICION_TRAMPAS_12JUN2022.xlsx"
-     subprocess.getoutput(bash_command)
-     bash_command = "md5sum tests/data/datos_formato_excel/IG_POSICION_TRAMPAS_12JUN2022.csv"
-     output = subprocess.getoutput(bash_command)
-     obtained_md5 = output.split(" ")[0]
-     expected_md5 = "4fd30809660722b7bef86b3216b00d30"
-     assert obtained_md5 == expected_md5
