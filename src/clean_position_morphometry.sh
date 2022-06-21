c9a1e0b4c432065632e0fbed49f96ac4ea99f2ec
Mario Villasante <mario.villasante@islas.org.mx>
2022-06-21 15:57:25 -0700

    🚧 Setting scripts

---
+++ src/clean_position_morphometry.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+#Convierte archivo xlsx a csv
+
+set -euo pipefail
+
+
+
+xlsx_2_csv(){
+    src_path=$(dirname "${1}")
+    table_name=$(basename "${1}" .xlsx)
+    file_name=${src_path}/${table_name}
+    in2csv --no-header-row --blanks ${1} | tail --lines=+2 > ${file_name}.tmp
+    csvcut -c 1-${2} -x ${file_name}.tmp > ${file_name}.csv
+}
+
+clean_position() {
+    number_columns=13
+    xlsx_2_csv ${1} ${number_columns}
+}
