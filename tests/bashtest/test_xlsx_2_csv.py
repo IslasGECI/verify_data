45bf81d89aab97c4c1361c9447da4f3cc004cc48
Mario Villasante <mario.villasante@islas.org.mx>
2022-06-22 09:50:16 -0700

    ♻ Extract function

--- tests/bashtest/test_xlsx_2_csv.py
+++ tests/bashtest/test_xlsx_2_csv.py
@@ -1,20 +1,15 @@
 import subprocess
 
 
-def test_clean_xlsx_2_csv():
-    number_columns_position = 13
-    bash_command = f"./src/clean_xlsx_2_csv.sh tests/data/datos_formato_excel/IG_POSICION_TRAMPAS_12JUN2022.xlsx {number_columns_position}"
-    subprocess.getoutput(bash_command)
-    bash_command = "md5sum tests/data/datos_formato_excel/IG_POSICION_TRAMPAS_12JUN2022.csv"
-    output = subprocess.getoutput(bash_command)
-    obtained_md5 = output.split(" ")[0]
-    expected_md5 = "4fd30809660722b7bef86b3216b00d30"
-    assert obtained_md5 == expected_md5
+
+def setup_xlsx_2_csv(type_data):
+    bash_command = f"rm --force tests/data/datos_formato_excel/IG_{type_data}_12JUN2022.csv" # TODO: Extraer a setup
+    subprocess.run(["bash", "-c", bash_command]) # TODO: Extraer a setup
 
 def test_clean_position():
     # Set up:
-    bash_command = "rm --force tests/data/datos_formato_excel/IG_POSICION_TRAMPAS_12JUN2022.csv" # TODO: Extraer a setup
-    subprocess.run(["bash", "-c", bash_command]) # TODO: Extraer a setup
+    type_data = "POSICION_TRAMPAS"
+    setup_xlsx_2_csv(type_data)
     # Carga la función clean_position() desde el archivo clean_xlsx_2_csv.sh
     bash_command = "source src/clean_position_morphometry.sh && clean_position tests/data/datos_formato_excel/IG_POSICION_TRAMPAS_12JUN2022.xlsx"
     subprocess.run(["bash", "-c", bash_command])
