92660ea14402f639a8f17084ed00b83ed9652f51
Mario Villasante <mario.villasante@islas.org.mx>
2022-06-22 09:23:30 -0700

    ✅ Pass  test

--- tests/bashtest/test_xlsx_2_csv.py
+++ tests/bashtest/test_xlsx_2_csv.py
@@ -14,10 +14,10 @@ def test_clean_xlsx_2_csv():
 def test_clean_position():
     # Set up:
     bash_command = "rm --force tests/data/datos_formato_excel/IG_POSICION_TRAMPAS_12JUN2022.csv" # TODO: Extraer a setup
-    output = subprocess.getoutput(bash_command) # TODO: Extraer a setup
+    subprocess.run(["bash", "-c", bash_command]) # TODO: Extraer a setup
     # Carga la función clean_position() desde el archivo clean_xlsx_2_csv.sh
     bash_command = "source src/clean_position_morphometry.sh && clean_position tests/data/datos_formato_excel/IG_POSICION_TRAMPAS_12JUN2022.xlsx"
-    subprocess.run(bash_command)
+    subprocess.run(["bash", "-c", bash_command])
     # Calcula el picadillo de la salida
     bash_command = "md5sum tests/data/datos_formato_excel/IG_POSICION_TRAMPAS_12JUN2022.csv"
     output = subprocess.getoutput(bash_command)
