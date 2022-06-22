f5ddf01b24025672ef0fb1c950a7bc5d8d6fd97c
Mario Villasante <mario.villasante@islas.org.mx>
2022-06-22 09:30:49 -0700

    🧪 Add failing tests

--- tests/bashtest/test_xlsx_2_csv.py
+++ tests/bashtest/test_xlsx_2_csv.py
@@ -24,3 +24,17 @@ def test_clean_position():
     obtained_md5 = output.split(" ")[0]
     expected_md5 = "4fd30809660722b7bef86b3216b00d30"
     assert obtained_md5 == expected_md5
+
+def test_clean_morphometry():
+    # Set up:
+    bash_command = "rm --force tests/data/datos_formato_excel/IG_MORFOMETRIA_GATOS_12JUN2022.csv" # TODO: Extraer a setup
+    subprocess.run(["bash", "-c", bash_command]) # TODO: Extraer a setup
+    # Carga la función clean_position() desde el archivo clean_xlsx_2_csv.sh
+    bash_command = "source src/clean_position_morphometry.sh && clean_morphometry tests/data/datos_formato_excel/IG_MORFOMETRIA_GATOS_12JUN2022.xlsx"
+    subprocess.run(["bash", "-c", bash_command])
+    # Calcula el picadillo de la salida
+    bash_command = "md5sum tests/data/datos_formato_excel/IG_MORFOMETRIA_GATOS_12JUN2022.csv"
+    output = subprocess.getoutput(bash_command)
+    obtained_md5 = output.split(" ")[0]
+    expected_md5 = "4fd30809660722b7bef86b3216b00d30"
+    assert obtained_md5 == expected_md5
