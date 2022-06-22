88bf9b2c417fbf548b9ea89d9b5b02e200b3849f
Mario Villasante <mario.villasante@islas.org.mx>
2022-06-22 10:40:57 -0700

    🚀 Run 'clean_position_morphometry.sh' in 'demo_workflow'

--- src/clean_position_morphometry.sh
+++ src/clean_position_morphometry.sh
@@ -9,7 +9,7 @@ set -euo pipefail
 xlsx_2_csv(){
     data_file=${1}
     number_columns=${2}
-    src_path=$(dirname "${data_file}")
+    local src_path=$(dirname "${data_file}")
     table_name=$(basename "${data_file}" .xlsx)
     file_name=${src_path}/${table_name}
     in2csv --no-header-row --blanks ${data_file} | tail --lines=+2 > ${file_name}.tmp
