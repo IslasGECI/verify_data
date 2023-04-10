545ca81c933a7d89318a5e87dd37aad699eb3d1b
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-04-10 12:47:02 -0700

    🧪💥 Fail test for complete tidy

--- diferenciasMorfometriaPosicionTrampas/tests/testthat/test_effort_2_tidy.R
+++ diferenciasMorfometriaPosicionTrampas/tests/testthat/test_effort_2_tidy.R
@@ -4,20 +4,21 @@ archivo_prueba <- "../data_tests/esfuerzo_data_tests.csv"
 datos_prueba <- readr::read_csv(archivo_prueba)
 archivo_prueba <- "../data_tests/esfuerzo_data_tests_2.csv"
 datos_prueba_2 <- readr::read_csv(archivo_prueba)
-archivo_prueba <- "../data_tests/esfuerzo_data_tests_3.csv"
-datos_prueba_3 <- readr::read_csv(archivo_prueba)
 archivo_prueba <- "../data_tests/esfuerzo_data_tests_sin_capturas.csv"
 datos_prueba_sin_capturas <- readr::read_csv(archivo_prueba)
 
 describe("Que la función pase al formato tidy", {
-  it("expected first output", {
-    salida_test <- data.frame(
-      ID_de_trampa = factor(rep(c("TC-20-1084", "TC-10-0980-MV"), each = 7)),
-      Estado_trampa = factor(c("A", "A", "X", "A", "A", "A", "A", "X", "A", "A", "A", "A", "D", "D")),
-      Fecha = factor(rep(c("4/27/2020", "4/28/2020", "4/29/2020", "4/30/2020", "5/1/2020", "5/2/2020", "5/3/2020"), 2))
-    )
+  it("expected number of columns", {
     obtained <- effort_2_tidy(datos_prueba)
-    expect_equal(obtained, salida_test)
+    obtained_columns <- colnames(obtained)
+    expected_columns <- c("ID_de_trampa", "Estado_trampa", "Fecha")
+    expect_equal(obtained_columns, expected_columns)
+  })
+  it("expected number of rows", {
+    obtained <- effort_2_tidy(datos_prueba)
+    obtained_rows <- nrow(obtained)
+    expected_rows <- 42
+    expect_equal(obtained_rows, expected_rows)
   })
   it("expected second output", {
     salida_test_2 <- data.frame(
@@ -28,22 +29,14 @@ describe("Que la función pase al formato tidy", {
     obtained <- effort_2_tidy(datos_prueba_2)
     expect_equal(obtained, salida_test_2)
   })
-  it("expected third output", {
-    salida_test_3 <- data.frame(
-      ID_de_trampa = factor(rep(c("TC-20-1084", "TC-20-1085", "TC-10-0980-MV"), each = 7)),
-      Estado_trampa = factor(c("A", "A", "X", "A", "A", "A", "A", "A", "A", "X", "A", "A", "X", "A", "X", "A", "A", "A", "A", "D", "D")),
-      Fecha = factor(rep(c("4/27/2020", "4/28/2020", "4/29/2020", "4/30/2020", "5/1/2020", "5/2/2020", "5/3/2020"), 3))
-    )
-    obtained <- effort_2_tidy(datos_prueba_3)
-    expect_equal(obtained, salida_test_3)
-  })
   it("expected output without captures", {
     salida_test_sin_capturas <- data.frame(
-      ID_de_trampa = "",
-      Estado_trampa = "",
-      Fecha = ""
+      ID_de_trampa = factor(rep(c("TC-10-0963-MP", "TC-10-0981-MV"), each = 7)),
+      Estado_trampa = factor(c("A", "A", "A", "A", "A", "D", "D", "A", "A", "A", "A", "A", "D", "D")),
+      Fecha = factor(rep(c("4/27/2020", "4/28/2020", "4/29/2020", "4/30/2020", "5/1/2020", "5/2/2020", "5/3/2020"), 2))
     )
     obtained <- effort_2_tidy(datos_prueba_sin_capturas)
     expect_equal(obtained, salida_test_sin_capturas)
+    expect_message(effort_2_tidy(datos_prueba_sin_capturas), "NO HAY CAPTURAS")
   })
 })
