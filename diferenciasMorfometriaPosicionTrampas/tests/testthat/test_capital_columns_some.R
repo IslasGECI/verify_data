7d965ca25e45a8935552def2a9b15b3578f14aca
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-11-07 11:04:23 -0800

    🛑🧪 Fail tests

--- diferenciasMorfometriaPosicionTrampas/tests/testthat/test_capital_columns_some.R
+++ diferenciasMorfometriaPosicionTrampas/tests/testthat/test_capital_columns_some.R
@@ -9,4 +9,11 @@ describe("Change to lower columns: edad, patron, estado de salud y reproductivo"
     expected_pattern_column <- c("solido", "atigrado", "atigrado", "atigrado", "atigrado", "atigrado", "solido")
     expect_equal(obtained$Patron, expected_pattern_column)
   })
+  it("Example 3: health and reproductive status", {
+    expected_health_column <- c(rep("muerto", 6), "vivo")
+    expect_equal(obtained$Estado_de_salud, expected_health_column)
+
+    expected_reproductive_column <- c(NA, rep("reproductivo", 6))
+    expect_equal(obtained$Estado_reproductivo, expected_reproductive_column)
+  })
 })
