9481c5c4d69b4454483b37b0a5b3b698a5a9800c
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-11-07 11:41:16 -0800

    🚧  Change to lowercase some columns

--- src/clean_morphometry.R
+++ src/clean_morphometry.R
@@ -5,6 +5,7 @@
 # Librarias necesarias
 library(optparse)
 library(tidyverse)
+library(diferenciasMorfometriaPosicionTrampas)
 
 option_list <- list(
   make_option(
@@ -26,6 +27,8 @@ opt_parser <- OptionParser(option_list = option_list)
 options <- parse_args(opt_parser)
 input_file <- options$data
 morphometry <- read_csv(input_file, show_col_types = FALSE)
-clean_morphometry <- morphometry %>% filter(Fecha > as.Date("2018-04-23"))
+clean_morphometry <- morphometry %>%
+  filter(Fecha > as.Date("2018-04-23")) %>%
+  lower_columns_age_patron_and_status()
 output_file <- options$out
 write_csv(clean_morphometry, output_file)
