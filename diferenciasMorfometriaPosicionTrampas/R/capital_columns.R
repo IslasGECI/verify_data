e3ff8e4c400e7c32a00202d5684f4965c97d3e75
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-11-07 11:19:05 -0800

    ♻️  Refactor

--- diferenciasMorfometriaPosicionTrampas/R/capital_columns.R
+++ diferenciasMorfometriaPosicionTrampas/R/capital_columns.R
@@ -1,9 +1,9 @@
 #' @export
 lower_columns_age_patron_and_status <- function(raw_data) {
   cleaned_data <- raw_data |>
-    mutate(Edad = stringr::str_to_lower(Edad)) |>
-    mutate(Estado_reproductivo = stringr::str_to_lower(Estado_reproductivo)) |>
-    mutate(Estado_de_salud = stringr::str_to_lower(Estado_de_salud)) |>
-    mutate(Patron = stringr::str_to_lower(Patron))
+    dplyr::mutate(Edad = stringr::str_to_lower(Edad)) |>
+    dplyr::mutate(Estado_reproductivo = stringr::str_to_lower(Estado_reproductivo)) |>
+    dplyr::mutate(Estado_de_salud = stringr::str_to_lower(Estado_de_salud)) |>
+    dplyr::mutate(Patron = stringr::str_to_lower(Patron))
   return(cleaned_data)
 }
