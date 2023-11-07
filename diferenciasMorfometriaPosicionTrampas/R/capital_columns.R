49415f89b16dfae89e2470d2bf1aa365c1a33516
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-11-07 10:59:13 -0800

    ✅ Pass tests

--- diferenciasMorfometriaPosicionTrampas/R/capital_columns.R
+++ diferenciasMorfometriaPosicionTrampas/R/capital_columns.R
@@ -1,5 +1,6 @@
 lower_columns_age_patron_and_status <- function(raw_data) {
   cleaned_data <- raw_data |>
-    mutate(Edad = stringr::str_to_lower(Edad))
+    mutate(Edad = stringr::str_to_lower(Edad)) |>
+    mutate(Patron = stringr::str_to_lower(Patron))
   return(cleaned_data)
 }
