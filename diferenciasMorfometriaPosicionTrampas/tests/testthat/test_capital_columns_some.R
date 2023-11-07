88d3419e78c27aed93da7661fbfa6636a8f2c373
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-11-07 10:49:20 -0800

    🛑🧪 Fail tests

--- diferenciasMorfometriaPosicionTrampas/tests/testthat/test_capital_columns_some.R
+++ diferenciasMorfometriaPosicionTrampas/tests/testthat/test_capital_columns_some.R
@@ -1,6 +1,8 @@
 describe("Change to lower columns: edad, patron, estado de salud y reproductivo", {
   it("Example 1", {
     raw_data <- readr::read_csv("/workdir/diferenciasMorfometriaPosicionTrampas/tests/data_tests/morfometria_capitalized.csv")
-    lower_columns_age_patron_and_status(raw_data)
+    obtained <- lower_columns_age_patron_and_status(raw_data)
+    expected_age_column <- c("adulto", "juvenil", "adulto", "adulto", "adulto", "adulto", "adulto")
+    expect_equal(obtained$Edad, expected_age_column)
   })
 })
