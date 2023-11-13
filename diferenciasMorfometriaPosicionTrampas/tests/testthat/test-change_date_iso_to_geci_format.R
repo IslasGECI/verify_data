57b135bb0a9c8f6227813c1752f86d7e5ecfb932
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-11-13 11:56:41 -0800

    🛑🧪 Fail tests

--- diferenciasMorfometriaPosicionTrampas/tests/testthat/test-change_date_iso_to_geci_format.R
+++ diferenciasMorfometriaPosicionTrampas/tests/testthat/test-change_date_iso_to_geci_format.R
@@ -23,4 +23,10 @@ describe("Change iso to geci format", {
     obtained <- change_date_iso_to_geci_format(date)
     expect_equal(obtained, expected)
   })
+  it("English to spanish example: August", {
+    date <- "2012-08-24"
+    expected <- "24/Ago/2012"
+    obtained <- change_date_iso_to_geci_format(date)
+    expect_equal(obtained, expected)
+  })
 })
