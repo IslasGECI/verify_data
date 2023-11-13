f08462e53f244d60de83ac4cf5e4b492154cb556
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-11-13 12:53:47 -0800

    ♻️  Refactor

--- diferenciasMorfometriaPosicionTrampas/R/change_date_iso_to_geci_format.R
+++ diferenciasMorfometriaPosicionTrampas/R/change_date_iso_to_geci_format.R
@@ -1,5 +1,6 @@
 change_date_iso_to_geci_format <- function(date) {
-  geci_format_date <- format(lubridate::ymd(date), "%d/%b/%Y") |>
+  geci_format_date <- lubridate::ymd(date) |>
+    format("%d/%b/%Y") |>
     .translate_months_name()
   return(geci_format_date)
 }
--- diferenciasMorfometriaPosicionTrampas/tests/testthat/test-change_date_iso_to_geci_format.R
+++ diferenciasMorfometriaPosicionTrampas/tests/testthat/test-change_date_iso_to_geci_format.R
@@ -11,28 +11,10 @@ describe("Change iso to geci format", {
     obtained <- change_date_iso_to_geci_format(date)
     expect_equal(obtained, expected)
   })
-  it("Third example", {
-    date <- "2023-01-30"
-    expected <- "30/Ene/2023"
-    obtained <- change_date_iso_to_geci_format(date)
-    expect_equal(obtained, expected)
-  })
-  it("English to spanish example: April", {
-    date <- "2023-04-16"
-    expected <- "16/Abr/2023"
-    obtained <- change_date_iso_to_geci_format(date)
-    expect_equal(obtained, expected)
-  })
-  it("English to spanish example: August", {
-    date <- "2012-08-24"
-    expected <- "24/Ago/2012"
-    obtained <- change_date_iso_to_geci_format(date)
-    expect_equal(obtained, expected)
-  })
-  it("English to spanish example: December", {
-    date <- "2012-12-24"
-    expected <- "24/Dic/2012"
-    obtained <- change_date_iso_to_geci_format(date)
+  it("English to spanish example:  January, April, August and December", {
+    dates <- c("2023-01-30", "2023-04-16", "2012-08-24", "2012-12-14")
+    expected <- c("30/Ene/2023", "16/Abr/2023", "24/Ago/2012", "14/Dic/2012")
+    obtained <- change_date_iso_to_geci_format(dates)
     expect_equal(obtained, expected)
   })
 })
