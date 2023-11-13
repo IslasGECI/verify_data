ba6697ff7ab288b8cb1078dc1c830e82a951709e
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-11-13 11:52:46 -0800

    ✅ Pass tests

--- diferenciasMorfometriaPosicionTrampas/R/change_date_iso_to_geci_format.R
+++ diferenciasMorfometriaPosicionTrampas/R/change_date_iso_to_geci_format.R
@@ -1,4 +1,5 @@
 change_date_iso_to_geci_format <- function(date) {
-  geci_format_date <- format(lubridate::ymd(date), "%d/%b/%Y")
+  geci_format_date <- format(lubridate::ymd(date), "%d/%b/%Y") |>
+    stringr::str_replace_all("Jan", "Ene")
   return(geci_format_date)
 }
