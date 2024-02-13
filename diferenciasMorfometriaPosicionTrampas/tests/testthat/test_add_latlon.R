4b93a4ad89a4c02004056ee3c9fc7f6468e00c82
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-02-13 11:39:56 -0800

    🛑🧪 Fail tests

---
+++ diferenciasMorfometriaPosicionTrampas/tests/testthat/test_add_latlon.R
@@ -0,0 +1,6 @@
+describe("Add latitude and longitude column", {
+  it("Add latitude and longitude column", {
+    utm_coordinates <- readr::read_csv("/workdir/diferenciasMorfometriaPosicionTrampas/tests/data_tests/morfometria_capitalized.csv", show_col_types = FALSE)
+    obtained <- add_latlon(utm_coordinates)
+  })
+})
