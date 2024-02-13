5c8cb8b09fd41f158db654be47b2c4f863225bc8
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-02-13 11:53:03 -0800

    🛑🧪 Fail tests

--- diferenciasMorfometriaPosicionTrampas/tests/testthat/test_add_latlon.R
+++ diferenciasMorfometriaPosicionTrampas/tests/testthat/test_add_latlon.R
@@ -4,5 +4,8 @@ describe("Add latitude and longitude column", {
     obtained <- add_latlon(utm_coordinates)
     added_columns <- c("latitude", "longitude")
     expect_true(all(added_columns %in% colnames(obtained)))
+    expected_latitude <- 29.038756
+    expected_longitude <- -118.268294
+    assert_equal(obtained[6, "latitude"], expected_latitude)
   })
 })
