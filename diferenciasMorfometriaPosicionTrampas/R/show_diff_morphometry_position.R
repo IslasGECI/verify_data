e58e8064124504e5203b623b9f6d2d7fa5a10b55
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-02-13 14:53:05 -0800

    ♻️  Refactor

--- diferenciasMorfometriaPosicionTrampas/R/show_diff_morphometry_position.R
+++ diferenciasMorfometriaPosicionTrampas/R/show_diff_morphometry_position.R
@@ -1,4 +1,5 @@
-write_diff_between_captures <- function(options) {
+#' @export
+write_diff_captures_between <- function(options) {
   left <- read_csv(options$left_data, show_col_types = FALSE)
   right <- read_csv(options$right_data, show_col_types = FALSE)
   differences <- anti_join(left, right, by = c("Fecha", "ID_de_trampa"))
--- diferenciasMorfometriaPosicionTrampas/tests/testthat/test-show_diff_morphometry_position.R
+++ diferenciasMorfometriaPosicionTrampas/tests/testthat/test-show_diff_morphometry_position.R
@@ -4,7 +4,7 @@ describe("CLI for differences in captures between morphometry and position traps
     data_2 <- "/workdir/tests/data_tests/cleaned_position_traps_test.csv"
     output_path <- "/workdir/diferenciasMorfometriaPosicionTrampas/tests/missing_captures_in_position.csv"
     options_for_testing <- list("right_data" = data_2, "left_data" = data_1, "output_path" = output_path)
-    write_diff_between_captures(options_for_testing)
+    write_diff_captures_between(options_for_testing)
     expect_true(testtools::exist_output_file(output_path))
     obtained <- readr::read_csv(output_path)
     expected_missing_captures <- 9
