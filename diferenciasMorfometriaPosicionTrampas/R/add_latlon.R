0c6541b4d3f1c1f23cf14339dbc085590be642f0
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-02-13 13:01:18 -0800

    ♻️  Refactor

--- diferenciasMorfometriaPosicionTrampas/R/add_latlon.R
+++ diferenciasMorfometriaPosicionTrampas/R/add_latlon.R
@@ -1,4 +1,5 @@
+#' @export
 add_latlon <- function(utm_coordinates) {
   lonlat <- oce::utm2lonlat(utm_coordinates$Coordenada_Este, utm_coordinates$Coordenada_Norte, zone = 11)
-  utm_coordinates |> mutate("latitude" = lonlat$latitude, "longitude" = lonlat$longitude)
+  utm_coordinates |> dplyr::mutate("latitude" = lonlat$latitude, "longitude" = lonlat$longitude)
 }
