cce45a8f9178f66f4ef9083064b791ac412b75f4
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2024-02-14 09:58:48 -0800

    🔥 remove redundant test

--- tests/bashtest/test_show_diff_morphometry_position.py
+++
@@ -1,8 +0,0 @@
-from bashtest import assert_equal_file, set_tests_from_make
-
-
-def test_show_diff_morphometry_position():
-    set_tests_from_make("./reports/tables/missing_captures_in_position.csv")
-    file_new = "./reports/tables/missing_captures_in_position.csv"
-    file_test = "./tests/data_tests/missing_captures_in_position_test.csv"
-    assert_equal_file(file_new, file_test)
