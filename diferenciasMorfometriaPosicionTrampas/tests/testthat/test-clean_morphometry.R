908c9c2328132cc33da59dc4fe05f0536ea1feb1
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-02-14 10:49:04 -0800

    ♻️  Refactor

--- diferenciasMorfometriaPosicionTrampas/tests/testthat/test-clean_morphometry.R
+++ diferenciasMorfometriaPosicionTrampas/tests/testthat/test-clean_morphometry.R
@@ -5,5 +5,8 @@ describe("CLI for clean_morphometry ", {
     options_for_testing <- list("data_path" = raw_path, "output_path" = output_path)
     write_cleaned_morphometry(options_for_testing)
     expect_true(testtools::exist_output_file(output_path))
+    obtained <- readr::read_csv(output_path, show_col_types = FALSE)
+    expected_rows <- 938 - 194
+    expect_equal(nrow(obtained), expected_rows)
   })
 })
