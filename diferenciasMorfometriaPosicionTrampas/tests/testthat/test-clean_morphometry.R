779700bb6f9a1a8f070de9039272ed5e01ee607d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-02-14 10:32:12 -0800

    🛑🧪 Fail tests

---
+++ diferenciasMorfometriaPosicionTrampas/tests/testthat/test-clean_morphometry.R
@@ -0,0 +1,9 @@
+describe("CLI for clean_morphometry ", {
+  it("Write csv", {
+    raw_path <- "/workdir/diferenciasMorfometriaPosicionTrampas/tests/data_test/morfometria_gatos_isla_guadalupe_ISO_tests.csv"
+    output_path <- "/workdir/diferenciasMorfometriaPosicionTrampas/tests/cleaned_morphometry_tests.csv"
+    options_for_testing <- list("data_path" = raw_path, "output_path" = output_path)
+    write_cleaned_morphometry(options_for_testing)
+    expect_true(testtools::exist_output_file(output_path))
+  })
+})
