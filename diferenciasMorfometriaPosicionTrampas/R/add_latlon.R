694b6b1db0120b450c0668dce89145cc871ceae5
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-03-13 12:58:58 -0700

    ✅ Pass tests

--- diferenciasMorfometriaPosicionTrampas/R/add_latlon.R
+++ diferenciasMorfometriaPosicionTrampas/R/add_latlon.R
@@ -1,3 +1,9 @@
+#' @export
+convert_to_latlon <- function(options) {
+
+}
+
+
 add_latlon <- function(utm_coordinates) {
   lonlat <- oce::utm2lonlat(utm_coordinates$Coordenada_Este, utm_coordinates$Coordenada_Norte, zone = 11)
   utm_coordinates |> dplyr::mutate("latitude" = lonlat$latitude, "longitude" = lonlat$longitude)
