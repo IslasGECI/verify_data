d6cc7d6c8c0207d3ddbbb97e6ba3069441ac7f33
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-07-09 12:45:48 -0700

    ✅ Pass tests

--- diferenciasMorfometriaPosicionTrampas/R/clean_morphometry.R
+++ diferenciasMorfometriaPosicionTrampas/R/clean_morphometry.R
@@ -7,6 +7,14 @@ write_cleaned_morphometry <- function(options) {
     readr::write_csv(output_file)
 }
 
+write_cleaned_socorro_morphometry <- function(options) {
+  output_file <- options$output_path
+  morphometry <- .read_morphometry_file(options)
+  morphometry |>
+    .clean_morphometry() |>
+    readr::write_csv(output_file)
+}
+
 .read_morphometry_file <- function(options) {
   input_file <- options$data_path
   morphometry <- readr::read_csv(input_file, show_col_types = FALSE)
