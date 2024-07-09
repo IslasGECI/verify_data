9a80a9c75c4f2c9304a7fa10a0d8623a20c6bc73
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-07-09 12:38:45 -0700

    ♻️  Refactor

--- diferenciasMorfometriaPosicionTrampas/R/clean_morphometry.R
+++ diferenciasMorfometriaPosicionTrampas/R/clean_morphometry.R
@@ -1,11 +1,15 @@
 #' @export
 write_cleaned_morphometry <- function(options) {
-  input_file <- options$data_path
   output_file <- options$output_path
-  morphometry <- readr::read_csv(input_file, show_col_types = FALSE)
+  morphometry <- .read_morphometry_file(options)
   morphometry |>
     dplyr::filter(Fecha > as.Date("2018-04-23")) |>
     lower_columns_age_patron_and_status() |>
     add_latlon() |>
     readr::write_csv(output_file)
+}
+
+.read_morphometry_file <- function(options) {
+  input_file <- options$data_path
+  morphometry <- readr::read_csv(input_file, show_col_types = FALSE)
 }
