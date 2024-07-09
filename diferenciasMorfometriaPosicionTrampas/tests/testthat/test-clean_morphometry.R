bb4f34cecf926bb235a71f79b1d6b00c08ee697a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-07-09 12:44:57 -0700

    🛑🧪 Fail tests

--- diferenciasMorfometriaPosicionTrampas/tests/testthat/test-clean_morphometry.R
+++ diferenciasMorfometriaPosicionTrampas/tests/testthat/test-clean_morphometry.R
@@ -1,6 +1,6 @@
 describe("CLI for clean_morphometry ", {
+  raw_path <- "/workdir/diferenciasMorfometriaPosicionTrampas/tests/data_tests/morfometria_gatos_isla_guadalupe_ISO_tests.csv"
   it("Write csv", {
-    raw_path <- "/workdir/diferenciasMorfometriaPosicionTrampas/tests/data_tests/morfometria_gatos_isla_guadalupe_ISO_tests.csv"
     output_path <- "/workdir/diferenciasMorfometriaPosicionTrampas/tests/cleaned_morphometry_tests.csv"
     options_for_testing <- list("data_path" = raw_path, "output_path" = output_path)
     write_cleaned_morphometry(options_for_testing)
@@ -8,6 +8,13 @@ describe("CLI for clean_morphometry ", {
     obtained <- readr::read_csv(output_path, show_col_types = FALSE)
     expected_rows <- 938 - 194
     expect_equal(nrow(obtained), expected_rows)
+    testtools::delete_output_file(output_path)
+  })
+  it("Write Socorro cleaned csv", {
+    output_path <- "/workdir/diferenciasMorfometriaPosicionTrampas/tests/cleaned_socorro_morphometry_tests.csv"
+    options_for_testing <- list("data_path" = raw_path, "output_path" = output_path)
+    write_cleaned_socorro_morphometry(options_for_testing)
+    expect_true(testtools::exist_output_file(output_path))
     testtools::delete_output_file(output_path)
   })
 })
