50f5fff7c8075a6db218d42d9b2843276e4e6284
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-07-10 10:29:01 -0700

    ♻️  Refactor

--- spec/clean_morphometry_spec.sh
+++ spec/clean_morphometry_spec.sh
@@ -1,11 +1,12 @@
 #shellcheck shell=sh
 
 Describe 'clean raw files'
-source_functions() { source /workdir/src/clean_position_morphometry.sh; }
-BeforeRun 'source_functions'
+  source_functions() { source /workdir/src/clean_position_morphometry.sh; }
+  checksum() { md5sum ${output} | cut -d " " -f1; }
+  BeforeRun 'source_functions'
   It 'clean morphometry'
+    output="tests/data/datos_formato_excel/IG_MORFOMETRIA_GATOS_12JUN2022.csv"
     target="/workdir/tests/data/datos_formato_excel/IG_MORFOMETRIA_GATOS_12JUN2022.xlsx"
-    checksum() { md5sum tests/data/datos_formato_excel/IG_MORFOMETRIA_GATOS_12JUN2022.csv | cut -d " " -f1; }
     expected_md5="7cd3b0bd106d08eec93527b2bf3c9c85"
     When run clean_morphometry ${target}
     The result of function checksum should eq ${expected_md5}
