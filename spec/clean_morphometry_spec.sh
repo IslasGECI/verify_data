9c25dda4b02fed3fd8c9e7289c87e616c29e7e9f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-07-10 10:30:26 -0700

    ♻️  Refactor

--- spec/clean_morphometry_spec.sh
+++ spec/clean_morphometry_spec.sh
@@ -3,7 +3,9 @@
 Describe 'clean raw files'
   source_functions() { source /workdir/src/clean_position_morphometry.sh; }
   checksum() { md5sum ${output} | cut -d " " -f1; }
+  clean_output() { rm --force ${output};}
   BeforeRun 'source_functions'
+  AfterRun 'clean_output'
   It 'clean morphometry'
     output="tests/data/datos_formato_excel/IG_MORFOMETRIA_GATOS_12JUN2022.csv"
     target="/workdir/tests/data/datos_formato_excel/IG_MORFOMETRIA_GATOS_12JUN2022.xlsx"
