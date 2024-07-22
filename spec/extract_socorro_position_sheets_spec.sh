bd1ccfd8dcc187cca90d52e1d1e24209ecb25b31
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-07-22 13:41:35 -0700

    🛑🧪 Fail tests

--- spec/extract_socorro_position_sheets_spec.sh
+++ spec/extract_socorro_position_sheets_spec.sh
@@ -2,12 +2,10 @@
 
 Describe 'Extract xlsx sheets to csv files'
   Include /workdir/src/extract_socorro_position_sheets.sh
-  clean_output() { rm --force ${output};}
-  BeforeEach 'clean_output'
-  AfterEach 'clean_output'
-  It 'cut socorro morphometry'
-    output="/workdir/tests/data/datos_formato_excel/IS_MORFOMETRIA_GATOS_JUN2024.csv"
+  count_output_files() { ls /workdir/tests/data/IS_POSICION_TRAMPAS_JUN2024*.csv | wc -l;}
+  It 'cut socorro position'
     input_path="/workdir/tests/data/datos_formato_excel/IS_POSICION_TRAMPA_JUN2024.xlsx"
     When call extract_socorro_position ${input_path}
+    The result of function count_output_files should eq 4
   End
 End
