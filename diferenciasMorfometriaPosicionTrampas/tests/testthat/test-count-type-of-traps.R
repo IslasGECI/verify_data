ed98c4a288f3a5a65be6255c0bbd2139d81ee54a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-02-11 14:07:27 -0800

    🛑🧪 Fail tests

--- diferenciasMorfometriaPosicionTrampas/tests/testthat/test-count-type-of-traps.R
+++ diferenciasMorfometriaPosicionTrampas/tests/testthat/test-count-type-of-traps.R
@@ -23,6 +23,7 @@ describe("Count type of traps on certain period", {
     obtained_number_of_TC <- obtained_2 |>
       dplyr::filter(type == "TC") |>
       pull(count)
+    testthat::skip("Gold")
     expect_equal(obtained_number_of_TC, expected_number_of_TC)
   })
   it("count_active_traps(): type TP", {
@@ -31,5 +32,23 @@ describe("Count type of traps on certain period", {
       dplyr::filter(type == "TP") |>
       pull(count)
     expect_equal(obtained_number_of_TC, expected_number_of_TC)
+  })
+})
+
+describe("add_type_column()", {
+  it("data 1", {
+    data <- tibble::tibble(
+      "ID_de_trampa" = c("TC-01-001-NA", "TP-01-001-NA", "TC-02-002-NA"),
+      "Estado_trampa" = "A",
+      "Fecha" = c("31/Ene/2025", "31/Ene/2025", "31/Ene/2025"),
+    )
+    obtained <- add_type_column(data)
+    expected <- tibble::tibble(
+      "ID_de_trampa" = c("TC-01-001-NA", "TP-01-001-NA", "TC-02-002-NA"),
+      "Estado_trampa" = "A",
+      "Fecha" = c("31/Ene/2025", "31/Ene/2025", "31/Ene/2025"),
+      "type" = c("TC", "TP", "TC")
+    )
+    expect_equal(obtained, expected)
   })
 })
