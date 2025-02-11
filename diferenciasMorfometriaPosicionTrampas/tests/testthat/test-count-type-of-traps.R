bf81706e6f44a672d421380586bce1edd7be7af7
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-02-11 14:17:02 -0800

    🛑🧪 Fail tests

--- diferenciasMorfometriaPosicionTrampas/tests/testthat/test-count-type-of-traps.R
+++ diferenciasMorfometriaPosicionTrampas/tests/testthat/test-count-type-of-traps.R
@@ -8,7 +8,7 @@ describe("Count type of traps on certain period", {
 
   data_2 <- tibble::tibble(
     "ID_de_trampa" = c("TC-01-001-NA", "TP-01-001-NA", "TP-02-002-NA"),
-    "Estado_trampa" = "A",
+    "Estado_trampa" = c("A", "D", "A"),
     "Fecha" = c("31/Ene/2025", "31/Ene/2025", "31/Ene/2025"),
   )
   obtained_2 <- count_active_traps(data_2)
@@ -26,8 +26,14 @@ describe("Count type of traps on certain period", {
     expect_equal(obtained_number_of_TC, expected_number_of_TC)
   })
   it("count_active_traps(): type TP", {
-    expected_number_of_TC <- 1
-    obtained_number_of_TC <- obtained |>
+    expected_number_of_TP <- 1
+    obtained_number_of_TP <- obtained |>
+      dplyr::filter(type == "TP") |>
+      pull(count)
+    expect_equal(obtained_number_of_TC, expected_number_of_TC)
+
+    expected_number_of_TP <- 1
+    obtained_number_of_TP <- obtained_2 |>
       dplyr::filter(type == "TP") |>
       pull(count)
     expect_equal(obtained_number_of_TC, expected_number_of_TC)
