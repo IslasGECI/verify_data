66c49b28e73785d689aefc42adffb6fc0b9b6477
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-02-11 14:11:19 -0800

    ✅ Pass tests

--- diferenciasMorfometriaPosicionTrampas/R/count-type-of-traps.R
+++ diferenciasMorfometriaPosicionTrampas/R/count-type-of-traps.R
@@ -1,3 +1,7 @@
 count_active_traps <- function(data) {
   tibble::tibble("count" = c(2, 1), "type" = c("TC", "TP"))
 }
+
+add_type_column <- function(data) {
+  data |> mutate(type = stringr::str_sub(ID_de_trampa, 1, 2))
+}
