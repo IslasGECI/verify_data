4e144323eea8afb30a8e6ba32727ee0c29edf4a0
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-02-11 14:15:13 -0800

    ♻️  Refactor

--- diferenciasMorfometriaPosicionTrampas/R/count-type-of-traps.R
+++ diferenciasMorfometriaPosicionTrampas/R/count-type-of-traps.R
@@ -1,10 +1,15 @@
 count_active_traps <- function(data) {
   data |>
     add_type_column() |>
-    dplyr::group_by(type) |>
-    dplyr::summarise(count = n())
+    count_by_type()
 }
 
 add_type_column <- function(data) {
   data |> mutate(type = stringr::str_sub(ID_de_trampa, 1, 2))
+}
+
+count_by_type <- function(data) {
+  data |>
+    dplyr::group_by(type) |>
+    dplyr::summarise(count = n())
 }
