19eeb948d58fd6f3ac215386d28c61c9cec4bb70
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-02-12 13:30:16 -0800

    ♻️  Refactor

--- diferenciasMorfometriaPosicionTrampas/R/cli.R
+++ diferenciasMorfometriaPosicionTrampas/R/cli.R
@@ -1,8 +1,8 @@
 #' @export
-write_type_of_traps <- function(options, date) {
+write_type_of_traps <- function(options) {
   output_file <- options$output_path
   traps <- readr::read_csv(options$data_path, show_col_types = FALSE)
   traps |>
-    count_active_traps_after_date(date) |>
+    count_active_traps_after_date(options$initial_date) |>
     readr::write_csv(output_file)
 }
--- diferenciasMorfometriaPosicionTrampas/tests/testthat/test-cli.R
+++ diferenciasMorfometriaPosicionTrampas/tests/testthat/test-cli.R
@@ -3,9 +3,9 @@ describe("Test cli for write_type_of_traps", {
     data_path <- "/workdir/diferenciasMorfometriaPosicionTrampas/tests/data_tests/traps_daily_status_guadalupe.csv"
     output_path <- "/workdir/diferenciasMorfometriaPosicionTrampas/tests/number_of_traps_by_type.csv"
     date <- "31/Ene/2025"
-    options <- list("data_path" = data_path, "output_path" = output_path)
+    options <- list("data_path" = data_path, "initial_date" = date, "output_path" = output_path)
     testtools::if_exist_remove(output_path)
-    write_type_of_traps(options, date)
+    write_type_of_traps(options)
     expect_true(testtools::exist_output_file(output_path))
   })
 })
