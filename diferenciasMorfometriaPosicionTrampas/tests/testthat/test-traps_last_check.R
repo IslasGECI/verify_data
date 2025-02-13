ae971548549e45c347049f7d02817c25ee7e4c3d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-02-13 14:32:03 -0800

    🛑🧪 Fail tests

--- diferenciasMorfometriaPosicionTrampas/tests/testthat/test-traps_last_check.R
+++ diferenciasMorfometriaPosicionTrampas/tests/testthat/test-traps_last_check.R
@@ -1,11 +1,11 @@
 describe("Filter last active traps", {
   data <- tibble::tibble(
-    "ID_de_trampa" = c("TL-99-999-NA", "TL-99-999-NA", "TL-77-999-NA"),
-    "Estado_trampa" = c("A", "D", "A"),
-    "Fecha" = c("01/Feb/2025", "02/Feb/2025", "01/Feb/2025")
+    "ID_de_trampa" = c("TL-99-999-NA", "TL-99-999-NA", "TL-99-999-NA", "TL-77-999-NA"),
+    "Estado_trampa" = c("A", "D", "A", "A"),
+    "Fecha" = c("01/Feb/2025", "02/Feb/2025", "03/Feb/2025", "01/Feb/2025")
   )
+  obtained <- last_active_trap(data)
   it("last_active_trap", {
-    obtained <- last_active_trap(data)
     expected_rows <- 2
     obtained_rows <- nrow(obtained)
     expect_equal(obtained_rows, expected_rows)
