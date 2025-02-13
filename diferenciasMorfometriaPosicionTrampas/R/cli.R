31322bb2f9e445a44cf2f604bfc1430572fc3277
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-02-13 14:43:48 -0800

    ✅ Pass tests

--- diferenciasMorfometriaPosicionTrampas/R/cli.R
+++ diferenciasMorfometriaPosicionTrampas/R/cli.R
@@ -1,4 +1,13 @@
 #' @export
+write_traps_last_check <- function(options) {
+  output_file <- options$output_path
+  traps <- readr::read_csv(options$data_path, show_col_types = FALSE)
+  traps |>
+    last_active_trap() |>
+    readr::write_csv(output_file)
+}
+
+#' @export
 write_type_of_traps <- function(options) {
   output_file <- options$output_path
   traps <- readr::read_csv(options$data_path, show_col_types = FALSE)
