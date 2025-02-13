ad86468a9a1c1101579b7ee0c4d99c7c890dac39
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-02-13 14:42:24 -0800

    🛑🧪 Fail tests

--- diferenciasMorfometriaPosicionTrampas/tests/testthat/test-cli.R
+++ diferenciasMorfometriaPosicionTrampas/tests/testthat/test-cli.R
@@ -10,3 +10,14 @@ describe("Test cli for write_type_of_traps", {
     testtools::if_exist_remove(output_path)
   })
 })
+
+describe("Cli command for traps last check", {
+  it("Write csv with traps last check", {
+    data_path <- "/workdir/diferenciasMorfometriaPosicionTrampas/tests/data_tests/traps_daily_status_guadalupe.csv"
+    output_path <- "/workdir/diferenciasMorfometriaPosicionTrampas/tests/active_traps.csv"
+    options <- list("data_path" = data_path, "output_path" = output_path)
+    testtools::if_exist_remove(output_path)
+    write_traps_last_check(options)
+    expect_true(testtools::exist_output_file(output_path))
+  })
+})
