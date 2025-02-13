644ef319ed618dea624118870cb583a22e7be6ef
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-02-13 14:37:49 -0800

    ✅ Pass tests

--- diferenciasMorfometriaPosicionTrampas/R/traps_last_check.R
+++ diferenciasMorfometriaPosicionTrampas/R/traps_last_check.R
@@ -1,5 +1,6 @@
 last_active_trap <- function(data) {
   data |>
     dplyr::filter(Estado_trampa == "A") |>
+    dplyr::arrange(dplyr::desc(dplyr::row_number())) |>
     dplyr::distinct(ID_de_trampa, .keep_all = TRUE)
 }
